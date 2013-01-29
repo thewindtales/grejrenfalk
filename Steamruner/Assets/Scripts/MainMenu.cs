78636aac0555a085e4f2117de9f4cba38f19c61c
Lavrenicus <billever@rambler.ru>
2013-01-29 19:11:34 +0400

    New_mainMenu
    Double 2

--- Steamruner/Assets/Scripts/MainMenu.cs
+++ Steamruner/Assets/Scripts/MainMenu.cs
@@ -13,8 +13,25 @@ public class MainMenu : MonoBehaviour
 	void OnGUI()
 	{
 		GUI.Label (new Rect(Screen.width/2,20, 100, 20), "Our Game",Guistyle_Menu);
-		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2,NewGame.width,NewGame.height),NewGame))
-		{};
+		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2-NewGame.height,NewGame.width,NewGame.height),NewGame))
+		{
+			Debug.Log ("New game selected");
+			Application.LoadLevel ("GameScene1");
+		};
+		if(GUI.Button (new Rect(Screen.width/2,Screen.height/2,Options.width,Options.height),Options))
+		{
+			Debug.Log ("Go to options");
+			
+		}
+		if(GUI.Button (new Rect(Screen.width/2,Screen.height/2+Exit.height,Exit.width,Exit.height),Exit))
+		{
+			Debug.Log ("Exit Game");
+			Application.Quit();
+			
+		}
+		CursorTrack ();
+		
+		
 	}
 
 	// Use this for initialization
@@ -25,5 +42,13 @@ public class MainMenu : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void CursorTrack()
+	{
+		Vector3 MousePos = Input.mousePosition;
+		float sizeCursor = Cursor.height;
+		Rect CursorRect = new Rect(MousePos.x, Screen.height-MousePos.y,sizeCursor,sizeCursor);
+		GUI.Label (CursorRect,Cursor);
+		
 	}
 }
