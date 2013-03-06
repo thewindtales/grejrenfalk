589478edadf4bbfe8f16aef2227a02692eae261a
Lavrenicus <billever@rambler.ru>
2013-03-06 16:23:54 +0400

    Ispravlena levaya hotba

--- Steamruner/Assets/Scripts/Char_motophis.cs
+++ Steamruner/Assets/Scripts/Char_motophis.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
-[RequireComponent(typeof(Player_control))]
 
 public class Char_motophis : MonoBehaviour 
 {
 	public int Speed = 10; //скорость персонажа
-	public int JumpHeight = 2;// высота прыжка, пока не используется
+	public int JumpHeight = 20;// высота прыжка, пока не используется
 	public bool jump;
 	public float in_walk;
 	
@@ -16,6 +15,7 @@ public class Char_motophis : MonoBehaviour
 	public AnimationClip Walk;
 	public AnimationClip Jump;
 	public Transform Self;
+	public bool Left = false;
 	
 	private Vector3 JumpVect;
 	private bool OnGround;
@@ -27,7 +27,7 @@ public class Char_motophis : MonoBehaviour
 	void Start () 
 	{
 		Self=transform;
-		rigidbody.freezeRotation=true;
+		rigidbody.freezeRotation=true;//Нечего падать на бок
 		animation.AddClip (Idle,"Idle");
 		animation.AddClip (Walk,"Walk");
 	}
@@ -57,22 +57,31 @@ public class Char_motophis : MonoBehaviour
 		animation.CrossFade ("Walk");
 		if(in_walk>0)
 		{
+			if(Left)
+			{
+				//Если смотрели налево, то теперь поворачиваемся направо
+				Left=false;
+				Self.rotation=Quaternion.Euler (new Vector3(0,0,0));
+			}
 			Vector3 forw =new Vector3 (0,rigidbody.velocity.y,in_walk*Speed);
 			rigidbody.velocity=forw;
 			animation.CrossFade ("Walk");
 		}
 		if(in_walk<0)
 		{
+			if (!Left)
+			{
+				Left=true;
+				Self.rotation=Quaternion.Euler(new Vector3(0,180,0));
+			}
 			Vector3 back =new Vector3 (0,rigidbody.velocity.y,in_walk*Speed);
 			rigidbody.velocity= back;
-			//animation.CrossFadeQueued ("Walk");
 		}
 	}
 	
 	void Jumping()
 	{
 		JumpVect =new Vector3(rigidbody.velocity.x,JumpHeight,rigidbody.velocity.z);
-		//JumpVect+=rigidbody.velocity;
 		rigidbody.velocity= JumpVect;
 	}
 }
